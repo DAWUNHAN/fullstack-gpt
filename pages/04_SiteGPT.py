81c70488737b3cfa1a4a99d3e1f7b096e0a4f0f7
D.A Dawun Han <dawun.han@gmail.com>
2024-03-26 23:54:01 +0900

    SiteGPT; Parsing functions

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -1,13 +1,46 @@
-from langchain.document_loaders import AsyncChromiumLoader
-from langchain.document_transformers import Html2TextTransformer
+from langchain.document_loaders import SitemapLoader, text
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 import streamlit as st
+
+
+def parse_page(soup):
+    header = soup.find("header")
+    footer = soup.find("footer")
+    if header:
+        header.decompose()
+    if footer:
+        footer.decompose()
+    return (
+        str(soup.get_text())
+        .replace("\n", " ")
+        .replace("\xa0", " ")
+        .replace("CloseSearch Submit Blog", "")
+    )
+
+
+@st.cache_data(show_spinner="Loading website...")
+def load_website(url):
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000,
+        chunk_overlap=200,
+    )
+    loader = SitemapLoader(
+        url,
+        filter_urls=[
+            r"^(.*\/blog\/).*",
+        ],
+        parsing_function=parse_page,
+    )
+    loader.requests_per_second = 2
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
 
 st.set_page_config(
     page_title="SiteGPT",
     page_icon="🖥️",
 )
 
-html2text_transformer = Html2TextTransformer()
 
 st.markdown(
     """
@@ -28,7 +61,9 @@ with st.sidebar:
 
 
 if url:
-    loader = AsyncChromiumLoader([url])
-    docs = loader.load()
-    transformed = html2text_transformer.transform_documents(docs)
-    st.write(docs)
+    if ".xml" not in url:
+        with st.sidebar:
+            st.error("Please write down a Sitemap URL.")
+    else:
+        docs = load_website(url)
+        st.write(docs)
